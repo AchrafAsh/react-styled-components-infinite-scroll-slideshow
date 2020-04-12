1769459a5d1266da4699cbcd5c3f42947d786e80
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-04-12 18:34:32 +0200

    Create standalone component with variables

--- src/App.js
+++ src/App.js
@@ -1,79 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const SlideShowWrapper = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  overflow: hidden;
-`;
-
-const InfiniteSlideShow = styled.div`
-  width: 150vw;
-  display: grid;
-  grid-template-columns: ${(props) =>
-    `repeat(${props.length}, ${150 / props.length}vw)`};
-  animation: ${(props) =>
-    props.right
-      ? `bannerMoveRight ${100 / props.speed}s linear infinite`
-      : `bannerMoveLeft ${100 / props.speed}s linear infinite`};
-  @keyframes bannerMoveLeft {
-    0% {
-      transform: translateX(0);
-    }
-    49% {
-      transform: translateX(-150vw);
-      opacity: 1;
-    }
-    50% {
-      opacity: 0;
-      transform: translateX(-150vw);
-    }
-    51% {
-      opacity: 0;
-      transform: translateX(150vw);
-    }
-    52% {
-      opacity: 1;
-      transform: translateX(150vw);
-    }
-    100% {
-      transform: translateX(0);
-    }
-  }
-  @keyframes bannerMoveRight {
-    0% {
-      opacity: 1;
-      transform: translateX(0);
-    }
-    49% {
-      transform: translateX(-150vw);
-    }
-    98% {
-      transform: translateX(-300vw);
-      opacity: 1;
-    }
-    99% {
-      transform: translateX(-300vw);
-      opacity: 0;
-    }
-    100% {
-      transform: translateX(0);
-      opacity: 0;
-    }
-  }
-`;
-const Image = styled.img`
-  grid-row: 1;
-  grid-column: ${(props) => props.col};
-  height: ${(props) => props.height};
-  width: ${(props) => props.width};
-  width: auto;
-  place-self: center center;
-`;
+import React, { useState } from 'react';
+import SlideShow from './SlideShow';
 
 function App() {
-  const imageList = [
+  const images = [
     {
       url:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
@@ -86,45 +15,58 @@ function App() {
       url:
         'https://raw.githubusercontent.com/styled-components/brand/master/styled-components.png',
     },
-    { url: 'https://avatars3.githubusercontent.com/u/12551863?s=400&v=4' },
+    // { url: 'https://avatars3.githubusercontent.com/u/12551863?s=400&v=4' },
   ];
+
+  const [speed, setSpeed] = useState(20);
+  const [imageHeight, setImageHeight] = useState('200px');
+  const [imageWidth, setImageWidth] = useState('auto');
 
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
-        boxSizing: 'border-box',
       }}
     >
-      <SlideShowWrapper length={imageList.length}>
-        <InfiniteSlideShow length={imageList.length} speed={2}>
-          {imageList &&
-            imageList.map((image, index) => (
-              <Image
-                height='200px'
-                width='auto'
-                src={image.url}
-                col={index + 1}
-                key={index}
-              />
-            ))}
-        </InfiniteSlideShow>
-        <InfiniteSlideShow length={imageList.length} right speed={2}>
-          {imageList &&
-            imageList.map((image, index) => (
-              <Image
-                height='200'
-                width='auto'
-                src={image.url}
-                col={index + 1}
-                key={index}
-              />
-            ))}
-        </InfiniteSlideShow>
-      </SlideShowWrapper>
+      <SlideShow
+        images={images}
+        speed={speed}
+        imageHeight={imageHeight}
+        imageWidth={imageWidth}
+      />
+      <div style={{ width: '70%' }}>
+        <div>
+          <label>Speed</label>
+          <input
+            style={{ margin: '10px' }}
+            type='text'
+            value={speed}
+            onChange={(e) => setSpeed(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Image height</label>
+          <input
+            style={{ margin: '10px' }}
+            type='text'
+            value={imageHeight}
+            onChange={(e) => setImageHeight(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Image width</label>
+          <input
+            style={{ margin: '10px' }}
+            type='text'
+            value={imageWidth}
+            onChange={(e) => setImageWidth(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
   );
 }
